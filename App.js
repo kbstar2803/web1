aa46d031874a034900bcc0d1e5d134ac09ec32a2
kbstar2803 <30790161+kbstar2803@users.noreply.github.com>
2019-11-10 19:14:07 +0900

    test

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { Header, Main, Footer } from './component';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
